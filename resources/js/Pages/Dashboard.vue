11e7be3df0dcef78e7d380d48d27d9f3f5bac8d4
“BrandonDKlassen” <b_klassen667@hotmail.com>
2023-01-20 15:48:38 -0500

    latest fixes

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -17,17 +17,12 @@ import { Head } from '@inertiajs/inertia-vue3';
 
 <div class="relative  flex flex-col md:flex-row items-center md:shadow-xl md:h-72 mx-2">
     
-    <div class="z-10 order-1 relative w-full">
-        <div class="img src='img/profile.jpg';"></div>
-        <div class="md:hidden absolute inset-0 h-full p-6 pb-6 flex flex-col-reverse justify-start items-start bg-gradient-to-b from-transparent via-transparent to-gray-900">
-            <h3 class="w-full font-bold text-2xl text-white leading-tight mb-2"></h3>
-            <img src="img/profile.jpg" />
-            <h4 class="w-full text-xl text-gray-100 leading-tight"></h4>
+    <div class="z-0 order-1 relative w-full md:w-2/5 h-80 md:h-full overflow-hidden rounded-lg md:rounded-none md:rounded-r-lg">
+            <div class="absolute inset-0 w-full h-full object-fill object-center bg-blue-400 bg-opacity-30 bg-cover bg-bottom" style="background-image: url(../img/profile.jpeg ); background-blend-mode: multiply;"></div>
+            <div class="md:hidden absolute inset-0 h-full p-6 pb-6 flex flex-col-reverse justify-start items-start bg-gradient-to-b from-transparent via-transparent to-gray-900">
+                
+            </div>           
         </div>
-        <svg class="hidden md:block absolute inset-y-0 h-full w-24 fill-current text-white -ml-12" viewBox="0 0 100 100" preserveAspectRatio="none">
-            <polygon points="50,0 100,0 50,100 0,100" />
-        </svg>
-    </div>
 
     <div class="z-10 order-2  w-full h-full flex items-center ">
         <div class="p-8 md:pr-18 md:pl-14 md:py-12 mx-2 md:mx-0 h-full w-full bg-white">
